ab6c29fa1d2341c1807eb350374116150883fe53
cruzayye <johnnyc010392@gmail.com>
2018-10-16 00:09:41 -0700

    seperated files for organization

---
+++ eventlisteners.js
@@ -0,0 +1,22 @@
+  $("#busmall").click(function() {
+    projectsArray[1].toHtml();
+    
+  });
+  $("#cocktail").click(function() {
+    projectsArray[0].toHtml();
+    
+  });
+  $("#cookiestore").click(function() {
+    projectsArray[2].toHtml();
+    
+  });
+  $("#quiz").click(function() {
+    projectsArray[3].toHtml();
+    
+  });
+
+
+  $(".menu").click(function() {
+    $("nav img").toggleClass('icon');
+    
+  });
