73a91b46032d0e6609f9539d36f9c44f6d12cf89
cruzayye <johnnyc010392@gmail.com>
2018-10-28 14:43:14 -0700

    organized files in order for server js and node files to work

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.get('/', function (request, response) {
+  response.sendFile('./index.html');// is this the right directory 
+
+})
+
+app.listen(PORT, function () {
+  // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`listening on ${PORT}`);
+});
