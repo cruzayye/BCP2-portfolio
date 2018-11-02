67a0105e39e6a84cdce1b8f82302e963ef2f3682
cruzayye <johnnyc010392@gmail.com>
2018-11-01 18:13:46 -0700

    class06 got a an error commiting to fix

---
+++ public/js/eventlisteners.js
@@ -0,0 +1,63 @@
+
+$("#project-list").on('click', 'div', function() {
+  $('#projectSection').children().hide();
+  $('#projectSection').css('display', 'block');
+
+  let data = $(this).data('project');//takes the value of the div ex: data-project = "Bus Mall"
+  console.log(data);
+  $('[data-project="' + data + '"]').show();
+  
+  //make a loop that goes over the data attr and matches the name of the array in order to display. 
+  projectsArray.forEach(function(project) {
+    if(project.name == data){
+      $('[data-project="' + data + '"]').show();
+      console.log(project);
+
+      
+    }
+    
+
+
+  });
+
+
+});
+// console.log(localStorage.rawData);
+
+
+
+
+ $(".menu").click(function(){
+   $("nav img").toggleClass('icon');
+ });
+
+
+
+
+
+
+
+// ======old code, this was too repetitive so theres an easier way to do this======
+ 
+ // $("#busmall").click(function() {
+  //   projectsArray[1].toHtml();
+    
+  // });
+  // $("#cocktail").click(function() {
+  //   projectsArray[0].toHtml();
+    
+  // });
+  // $("#cookiestore").click(function() {
+  //   projectsArray[2].toHtml();
+    
+  // });
+  // $("#quiz").click(function() {
+  //   projectsArray[3].toHtml();
+    
+  // });
+
+
+  // $(".menu").click(function() {
+  //   $("nav img").toggleClass('icon');
+    
+  // });
