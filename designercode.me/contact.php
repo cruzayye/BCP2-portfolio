408d8743ea6d816b4438b574847a06ad29b8aaee
cruzayye <johnnyc010392@gmail.com>
2019-09-23 17:22:15 -0700

    mail is sending need ot syle form

--- designercode.me/contact.php
+++ designercode.me/contact.php
@@ -4,13 +4,11 @@ if(isset($_POST['submit'])){
   $email = $_POST['email'];
   $message = $_POST['message'];
 
-  //gmail blocks php emails need to replace email
-  $mailTo = "johnnyc010392@gmail.com";
-  $headers = "from: ".$email;
+  $mailTo = "mail@designercode.me";
+  $headers = "From: ".$email;
   $txt = "You have received an email form ".$name.".\n\n".$message;
 
-
   mail($mailTo, $subject, $txt, $headers);
-  header("Location: index.php?mailsend");
+  header("Location: index.html?mailsend");
 }
 ?>
